95542140640485b3fd578e4da65fbc841ab26ef3
MaximProsin <maximprosin647@gmail.com>
2024-07-21 19:45:57 +0300

    add postgresDB

---
+++ backend/DataAccess/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.DataAccess;
+
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+        
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("UsersDbConnection"));
+    }
+}
---
+++ backend/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models;
+
+public class User
+{
+    public Guid Id { get; set; } required   
+        public string UserName { get; set; } required 
+        public string Email { get; set; } required 
+        public string Password { get; set; }
+}
